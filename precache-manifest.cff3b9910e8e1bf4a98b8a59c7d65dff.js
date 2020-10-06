18eccd3b4ff3f1f111b4033a35507006a507f044
Edward <edwardbenedict92@gmail.com>
2020-10-06 14:10:59 +0300

    Updates

---
+++ precache-manifest.cff3b9910e8e1bf4a98b8a59c7d65dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a29496ee1d0ed7badb3f66b0f744dd",
+    "url": "/book-app-reactjs/index.html"
+  },
+  {
+    "revision": "4744f7fffc1c92f3b298",
+    "url": "/book-app-reactjs/static/css/main.b3bcdef8.chunk.css"
+  },
+  {
+    "revision": "93a47ecbb3a4851272ac",
+    "url": "/book-app-reactjs/static/js/2.e946e90c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/book-app-reactjs/static/js/2.e946e90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4744f7fffc1c92f3b298",
+    "url": "/book-app-reactjs/static/js/main.aa97ffba.chunk.js"
+  },
+  {
+    "revision": "a24d04c26841037fd347",
+    "url": "/book-app-reactjs/static/js/runtime-main.107e0293.js"
+  }
+]);
